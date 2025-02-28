3691d8f6874d96da3c743eb3fd324a6bebaa16bb
Way-Duy <duyduyyud@yahoo.com>
2025-02-28 16:49:42 +0900

    Initial commit

---
+++ src/client/kotlin/snagtype/bingobongo/MinecraftBingoClient.kt
@@ -0,0 +1,9 @@
+package snagtype.bingobongo
+
+import net.fabricmc.api.ClientModInitializer
+
+object MinecraftBingoClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/snagtype/bingobongo/MinecraftBingo.kt
@@ -0,0 +1,15 @@
+package snagtype.bingobongo
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object MinecraftBingo : ModInitializer {
+    private val logger = LoggerFactory.getLogger("minecraft-bingo")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
